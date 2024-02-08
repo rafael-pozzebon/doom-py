146d97374dff653c44621bb42399d10744512a5d
Rafael Pozzebon <63304560+rafael-pozzebon@users.noreply.github.com>
2024-02-07 23:04:01 -0300

    initial commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+# game settings
+RES = WIDTH, HEIGHT = 1600, 900
+FPS = 60
+
+# map settings
+PLAYER_POS = 1.5, 5         # player's initial position
+PLAYER_ANGLE = 0            # player's initial angle
+PLAYER_SPEED = 0.004        # player's speed
+PLAYER_ROT_SPEED = 0.002    # player's rotation speed
